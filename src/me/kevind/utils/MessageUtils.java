9e0fbde548964bb61b9b1e814501c9f3b8a26ecc
Kevin D <kevin.diangelis@gmail.com>
2023-01-16 18:14:55 -0500

    Update MessageUtils.java

--- src/me/kevind/utils/MessageUtils.java
+++ src/me/kevind/utils/MessageUtils.java
@@ -4,9 +4,9 @@ import org.bukkit.command.CommandSender;
 
 public class MessageUtils {
     public static void message(CommandSender sender, String message) {
-        sender.sendMessage(message);
+        sender.sendMessage(ColorUtils.color(message));
     }
     public static void sendMessage(CommandSender sender, String message) {
-        sender.sendMessage(message);
+        sender.sendMessage(ColorUtils.color(message));
     }
 }
